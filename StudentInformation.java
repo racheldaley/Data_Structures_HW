59666361671ce73bd024f0f013e554b383c5bac8
racheldaley <rachel.daley95@gmail.com>
2018-09-16 19:45:32 -0400

    Update StudentInformation.java

--- StudentInformation.java
+++ StudentInformation.java
@@ -9,35 +9,41 @@ public class StudentInformation
    final int STU_SIZE = Integer.parseInt(inputString);
    int option;
 
-   SortedArray[] abcCollege = new SortedArray[STU_SIZE];
+         SortedArray abcCollege = new SortedArray();
+         /*StudentListing [] studentList = new StudentListing[STU_SIZE];
             for(i=0; i<STU_SIZE; ++i) // For loop adds values to the members of the array
            {
-              abcCollege[i] = new SortedArray();
-              abcCollege[i].input(); //allows user to input array member information
-           }
-         inputString = JOptionPane.showInputDialog("Please select an option from the following menu: \n1. Insert a new student's information" +
+              studentList[i] = new StudentListing();
+              studentList[i].input(); //allows user to input array member information
+           }*/
+           inputString = JOptionPane.showInputDialog("Please select an option from the following menu: \n1. Insert a new student's information" +
             "\n2. Fetch and Output a students information " + "\n 3.Delete a student's information" + "\n4. Update a student's information " +
-            "\n5. Output all student information in a sorted order " + "\6. Exit the program");
+            "\n5. Output all student information in a sorted order " + "\n6. Exit the program");
          option = Integer.parseInt(inputString);
         if (option == 1)
         {
-         abcCollege.insert();
+         StudentListing myData = new StudentListing();
+         System.out.println(abcCollege.insert(myData));
         }
         else if (option == 2)
         {
-         abcCollege[i].fetch();
+         String name = JOptionPane.showInputDialog("Who are you looking for?");
+         System.out.println(abcCollege.fetch(name));
         }
         else if (option == 3)
         {
-         abcCollege[i].delete();
+         String remove = JOptionPane.showInputDialog("Who would you like to remove?");
+         abcCollege.delete(remove);
         }
         else if (option == 4)
         {
-         abcCollege[i].update();
+         String newInfo = JOptionPane.showInputDialog("Who would you like to add?");
+         StudentListing updateInfo = new StudentListing();
+         abcCollege.update(newInfo, updateInfo);
         }
         else if (option == 5)
         {
-         abcCollege[i].showAll();
+         abcCollege.showAll();
         }
         else
         {
